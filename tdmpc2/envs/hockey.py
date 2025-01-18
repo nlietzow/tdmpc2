97a59061131f8b7490382f6b0f5112e62a6caa89
Niclas Lietzow <niclas.lietzow@outlook.com>
2025-01-18 07:04:52 +0100

    add hockey env

---
+++ tdmpc2/envs/hockey.py
@@ -0,0 +1,24 @@
+import torch
+
+from envs.wrappers.timeout import Timeout
+from hockey import hockey_env
+
+
+class HockeyEnvAdapter(hockey_env.HockeyEnv_BasicOpponent):
+    def step(self, action):
+        obs, reward, done, truncated, info = super().step(action)
+        return obs, reward, done or truncated, info
+
+    def reset(self, one_starting=None, mode=None, seed=None, options=None):
+        (obs, _) = super().reset(one_starting, mode, seed, options)
+        return torch.from_numpy(obs).float()
+
+
+def make_env(cfg):
+    if cfg.task != "hockey":
+        raise ValueError("Unknown task:", cfg.task)
+
+    env = HockeyEnvAdapter()
+    env = Timeout(env, max_episode_steps=500)
+
+    return env
